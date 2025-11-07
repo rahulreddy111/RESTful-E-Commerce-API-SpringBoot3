285867034fe47332de3af4cf1d6c017426b6ce5a
rahulreddy111 <71213343+rahulreddy111@users.noreply.github.com>
2025-11-07 17:06:43 +0530

    Update EcommerceApplication.java

--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -2,12 +2,19 @@ package com.example.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EcommerceApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
 }
+
